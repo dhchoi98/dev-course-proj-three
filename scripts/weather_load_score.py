34d18fb85fad73eb4ad4750f5d4962532de414a3
jinyoung0711 <ssconvergence2@gmail.com>
2025-06-10 22:37:27 +0900

    feat: 날씨 점수 적재 방식 MERGE로 변경 (중복 방지 및 업데이트 지원)

--- scripts/weather_load_score.py
+++ scripts/weather_load_score.py
@@ -1,27 +1,54 @@
 from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
-from scripts.weather_utils import format_simple_datetime
+from pendulum import parse
 
 def insert_weather_score(**kwargs):
     hook = SnowflakeHook(snowflake_conn_id="my_snowflake_conn")
 
-    # ✅ 실행 날짜 포맷팅
-    execution_date = kwargs["execution_date"]  # datetime 형식
+    # datetime 형식
+    execution_date = kwargs["execution_date"]
+    if isinstance(execution_date, str):
+        execution_date = parse(execution_date)
 
-    insert_sql = f"""
-        INSERT INTO WEATHER_DB.JINYOUNG.WEATHER_SCORE (
-            region_code, region_name, date, avg_temp, wind_speed, precipitation, score
-        )
-        VALUES (
-            '{kwargs["region_code"]}', '{kwargs["region_name"]}', '{execution_date}',
-            {kwargs["avg_temp"]}, {kwargs["wind_speed"]},
-            {kwargs["precipitation"]}, {kwargs["score"]}
-        )
+    date_str = execution_date.strftime("%Y-%m-%d")
+
+    merge_sql = f"""
+    MERGE INTO WEATHER_DB.JINYOUNG.WEATHER_SCORE tgt
+    USING (
+        SELECT
+            '{kwargs["region_code"]}'   AS region_code,
+            '{kwargs["region_name"]}'   AS region_name,
+            '{date_str}'          AS date,
+            {kwargs["avg_temp"]}        AS avg_temp,
+            {kwargs["wind_speed"]}      AS wind_speed,
+            {kwargs["precipitation"]}   AS precipitation,
+            {kwargs["humidity"]}        AS humidity,
+            {kwargs["ground_temp"]}     AS ground_temp,
+            {kwargs["wind_dir"]}        AS wind_dir,
+            {kwargs["score"]}           AS score
+    ) src
+    ON tgt.region_code = src.region_code
+        AND tgt.date        = src.date
+    WHEN MATCHED THEN
+        UPDATE SET
+        region_name  = src.region_name,
+        avg_temp     = src.avg_temp,
+        wind_speed   = src.wind_speed,
+        precipitation= src.precipitation,
+        humidity     = src.humidity,
+        ground_temp  = src.ground_temp,
+        wind_dir     = src.wind_dir,
+        score        = src.score
+    WHEN NOT MATCHED THEN
+        INSERT (region_code, region_name, date, avg_temp, wind_speed, precipitation, humidity, ground_temp, wind_dir, score)
+        VALUES (src.region_code, src.region_name, src.date, src.avg_temp, src.wind_speed, 
+        src.precipitation, src.humidity, src.ground_temp, src.wind_dir, src.score)
+    ;
     """
 
     conn = hook.get_conn()
     cursor = conn.cursor()
     try:
-        cursor.execute(insert_sql)
+        cursor.execute(merge_sql)
         conn.commit()
     finally:
         cursor.close()
